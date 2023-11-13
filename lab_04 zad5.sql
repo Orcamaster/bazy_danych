1b74524f70ca9e4f29b32418b0670b492d106388
Orcamaster <147385614+Orcamaster@users.noreply.github.com>
2023-11-13 11:56:31 +0100

    update

--- lab_04 zad5.sql
+++ lab_04 zad5.sql
@@ -41,3 +41,49 @@ delete from izba where nazwa_izby='spizarnia';
 # 5.9
 drop table izba;
 
+
+
+
+
+# lab_05
+
+# 1/a
+select * from postac where rodzaj='wiking' order by wiek DESC;
+delete from postac where id_postaci=6;
+delete from postac where id_postaci=7;
+
+# 1/b
+# krok 1
+# usunięcie kluczy obcych, które łączą się z kolumną id_postaci
+#tabela: walizka, przetwory
+alter table walizka drop foreign key walizka_ibfk_1;
+alter table przetwory drop foreign key przetwory_ibfk_1;
+alter table przetwory drop foreign key przetwory_ibfk_2;
+
+# krok 2
+# pozbycie się auto_increment
+alter table postac change id_postaci id_postaci int;
+#lub
+alter table postac modify id_postaci int;
+
+# krok 3
+alter table postac drop primary key;
+desc postac;
+
+# 2/a
+# dodanie kolumny jako pierwszej na liście kolumn w tabeli
+# dodanie kolumny z nowym kluczem głównym w tabeli z danymi
+# prawdopodobnie sie nie powiedzie - wartości null dla całej
+# kolumny nie są przecież unikalne, a to warunek klucza gł.
+alter table postac add pesel char(11) first;
+
+update postac set pesel=92839405112 + id_postaci;
+# dodanie primary key do tabeli postac
+alter table postac add primary key(pesel);
+desc postac;
+
+# 2/c
+alter table postac modify rodzaj enum('wiking','ptak','kobieta','syrena');
+
+
+
